4ed8f28758d28b9f6cfcc2838de7238ef832204d
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-03-16 21:20:32 +0300

    Vector
    add new vector

--- Prepod-BW316/Prepod-BW316.cpp
+++ Prepod-BW316/Prepod-BW316.cpp
@@ -1,12 +1,15 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <Windows.h>
-#include <stdio.h>
-#include <io.h>
-#include "HeadFunction.h"
+#include <vector>
 using namespace std;
-#define SHOW INTEGER
 
+class Human
+{
+public:
+    string name;
+    int age;
+};
 
 int main()
 {
@@ -14,34 +17,108 @@ int main()
     SetConsoleCP(1251);
     setlocale(LC_ALL, "Rus");
     srand(time(NULL));
+    int size = 10;
 
-    const int size = 10;
-#ifdef SHOW
-    int arr[size];
-    ShowArr(arr, size);
-    cout << Min(arr, size) << endl;
-#endif
+    // Инициализация Вектора
+    vector<Human> number; // Пустой
+    vector<Human> number_1(number); // Копия number
+    vector<Human> number_2 = number; // Копия number (присвоеный)
+    vector<Human> number_3(size); // Состоит из 10 чисел которые равны 0
+    vector<int> number_4(10, 5); // Состоит из 10 чисел которые равны 5
+    vector<int> number_5{ 5,4,3,2,1 }; // Состоит из чисел 5,4,3,2,1
+    vector<int> number_6 = { 1,2,3,4,5 }; // Состоит из чисел 1,2,3,4,5
 
-   
+    number_3[0].age = 25;
+    number_3[0].name = "Арман";
 
+    cout << number_3.at(0).age << endl;
+    cout << number_3.at(0).name << endl;
 
-    double mass[size];
-    ShowArr(mass, size);
-    cout << Min(mass, size) << endl;
-   
-  
-	return 0;
+    number_4.at(0) = 40;
+    number_4[1] = 3;
+    cout << number_4.front() << endl;
+    number_4.at(9) = 99;
+    cout << number_4.back() << endl;
+
+    vector<int> number_7;
+    number_7.push_back(55);
+    number_7.push_back(77);
+    number_7.push_back(22);
+    for (int n : number_7)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+
+    number_6.emplace_back(2000);
+    for (int n : number_6)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+
+    auto ipop = number_6.cbegin();
+    number_6.emplace(ipop + 4, 3000);
+    for (int n : number_6)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+
+    number_6.insert(ipop + 2,3, 9000);
+    for (int n : number_6)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+
+    auto begin = number_6.cend();
+    number_6.insert(begin, { 5,5,5 });
+    for (int n : number_6)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+
+    auto begin1 = number_5.cbegin();
+    number_5.erase(begin1+2);
+    for (int n : number_5)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+    auto first = number_6.cbegin();
+    auto last = number_6.cend();
+    number_6.erase(first + 3, last - 1);
+    for (int n : number_6)
+    {
+        cout << n << ' ';
+    }
+    return 0;
 }
+/* Обращение к элементам объекта vector
+* 1. [index] - По индексу в квадратных скобках
+* 2. .at(index) - Через функцию встроенную в vector
+* 3. .front() - Через функцию которая указывает на первый элемент
+* 4. .back() - Через функцию которая указывает на последний элемент
+*/
 
-//1. r - открытие файла только для ЧТЕНИЯ. Если его нет, то будет ошибка
-//2. w - открытие файла только для ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены и перезаписаны
-//3. a - открытие файла для Дополнения. Если файла нет, то будет создан. Если там есть данные то они будут удалены
-//4. r+ - открытие файла для ЧТЕНИЯ и ЗАПИСИ без возможности изменения размера файла. Если файла нет, то будет ошибка.
-//5. w+ - открытие файла для ЧТЕНИЯ и ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены и перезаписаны
-//6. a+ - открытие файла для ЧТЕНИЯ и ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены
-//7. rb - открытие двоичного файла для ЧТЕНИЯ. Если его нет, то будет ошибка.
-//8. wb - открытие двоичного файла для ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены и перезаписаны
-//9. b - открытие двоичного файла для Дополнения. Если файла нет, то будет создан. Если там есть данные то они будут удалены
-//10. r+b или b+ - открытие двоичного файла для ЧТЕНИЯ и ЗАПИСИ без возможности изменения размера файла. Если файла нет, то будет ошибка.
-//11. w+b или wb+ - открытие двоичного файла только для ЧТЕНИЯ и ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены и перезаписаны
-//12. a+b или ab+ -  открытие двоичного файла для ЧТЕНИЯ и ЗАПИСИ. Если файла нет, то будет создан. Если там есть данные то они будут удалены
+/* Добавление элементов в vector
+* 1. .push_back() - Добавляет необходимый элемент в конец
+* 2. .emplace_back() - Добавляет необходимый элемент в конец
+* 3. .emplace(post, value) - Вставляет элемент value на место post
+* *.emplace(post + 4, 3000); -  константный указатель на первый элемент + номер индекса
+* 4. .insert(post, value) - Копия .emplace(post, value)
+* 5. .insert(post, n, value) - ставит n кол-во элементов value на место post
+* 6. .insert(post, values) - ставит на место post список элементов values
+*/
+
+/* Удаление элементов из vector
+* 1. .clear() - удаляет все элементы
+* 2. .pop_back() - удаляет последний элемент
+* 3. .erase(p) - удаление по итератору (указателю на элемент) + индекс
+* 4. .erase(first, last) - удаляет элементы в жиапазоне от first до last
+*/
+
+//.cbegin() - создает константный указатель на первый элемент делая его итератором
+//.cend() - создает константный указатель на последний элемент делая его итератором
