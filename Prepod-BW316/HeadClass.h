a68cd35a270841fdea4112c68afda5e8170c20b6
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-03-23 20:36:01 +0300

    ООП Аксессоры / мутаторы
    Обновление файла для студентов

---
+++ Prepod-BW316/HeadClass.h
@@ -0,0 +1,29 @@
+#pragma once
+class Student
+{
+	char* full_name;
+	int markCount;
+	int* marks;
+public:
+	
+	//Set'эры
+	void createName(const char* studentName);
+	void setName(const char* studentName);
+	void setMarks(int mark, int index);
+
+	//Get'эры
+	const char* getName() { return full_name; }
+	int getMarks(int index) { return marks[index]; }
+
+	//Конструкторы
+
+	Student(); // конструктор по умолчанию
+	Student(const char* studentName);
+	Student(const char* studentName,
+		const int studentMarkCount);
+	Student(const char* studentName,
+		const int studentMarkCount,
+		const int* studentMarks);
+	//Обычные методы
+	double getAver();
+};
