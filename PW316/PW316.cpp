94bdf577a1095bbba3a6fe8e8fa9253e568f4851
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-04-11 21:37:15 +0300

    Para
    +

--- PW316/PW316.cpp
+++ PW316/PW316.cpp
@@ -1,49 +1,11 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #include <vector>
+#include <cassert>
 #include "HeadClass.h"
 using namespace std;
 
-/*class Point
-{
-	double x;
-	double y;
-public:
-	Point(double x, double y) : x{x}, y{x}{}
-	void display() const
-	{
-		cout << "(" << x << "," << y << ")" <<endl;
-	}
-	
-	Point operator+(const Point& point1)
-	{
-		return Point(x + point1.x, y + point1.y);
-	}
-	friend Point operator-(const Point& point1)
-	{
-		return Point(-point1.x, -point1.y);
-	}
-	friend ostream& operator<<(ostream& output, const Point& point1)
-	{
-		output << "(" << point1.x << "," << point1.y << ")";
-		return output;
-	}
-	friend istream& operator>>(istream& input, Point& point1)
-	{
-		input >> point1.x;
-		input >> point1.y;
-		return input;
-	}
-	double getX()const { return x; }
-	double getY()const { return y; }
-};
-
-Point operator+(const Point& point1, const Point& point2)
-{
-	return Point(point2.getX() + point1.getX(), point2.getY() + point1.getY());
-}*/
-
-class DynArray
+/*class DynArray
 {
 	int* arr;
 	int size;
@@ -58,12 +20,7 @@ public:
 	DynArray(const DynArray& object)
 		: arr{ new int[object.size] }, size{ object.size }
 	{
-		/* В списке инициализаторов полей класса выше
-		выделяем новый блок динамической памяти того же
-		размера, что и в копируемом экземпляре класса
-		DynArray. Следующим циклом копируем элементы
-		из оригинального блока памяти во вновь
-		выделенный. */
+		
 		for (int i{ 0 }; i < size; ++i)
 		{
 			arr[i] = object.arr[i];
@@ -72,7 +29,7 @@ public:
 			<< size << " elements, for " << this
 			<< '\n';
 	}
-	DynArray( DynArray&& object) : 
+	DynArray(DynArray&& object) :
 		arr{ object.arr}, size{ object.size}
 	{
 		object.arr = nullptr;
@@ -86,43 +43,39 @@ public:
 		// проверка на самоприсваивание
 		if (!(this == &object))
 		{
-			/* проверяем на невозможность "переиспользовать"
-			блок памяти, выделенный под имеющийся
-			массив */
+			
 			if (size != object.size)
 			{
-				/* в случае невозможности "переиспользования"
-				необходимо освободить память, УЖЕ
-				занимаемую элементами текущего
-				динамического массива */
+				
 				delete[] arr;
-				/* выделяем новый блок памяти согласно
-				размеру копируемого массива */
+				
 				arr = new int[object.size];
 			}
 			size = object.size;
-			/* Следующим циклом копируем элементы
-			из оригинального блока памяти во вновь
-			выделенный */
+			
 			for (int i = 0; i < size; i++)
 			{
 				arr[i] = object.arr[i];
 			};
-			/*int* dest{arr};//указатель на НАчало массива
-			int* src{ object.arr }; //указатель на начало object массива 
-			int* const end{ arr + size }; //указатель на конец массива
-			//выполнять пока "Начало" меньше чем "конец"
-			while (dest < end)
-			{
-				*dest++ = *src++;
-				//Копируем значения из object массива в наш массив
-			}*/
+			
 
 		}
 		std::cout << "DynArr copy assigned for "
 			<< size << " elements, for " << this
 			<< '\n';
 		return *this;
+	}
+	int operator[](int idx) const
+	{
+		//Константная перегрузка, возвращающая элемент по значению
+		assert(idx >= 0 and idx < size and "Index is out of range!");
+		return arr[idx];
+	}
+	int& operator[](int idx)
+	{
+		//Не Константная перегрузка, возвращающая элемент по ссылке
+		assert(idx >= 0 and idx < size and "Index is out of range!");
+		return arr[idx];
 	}
 	int getElem(int idx)const { return arr[idx]; }
 	void setElem(int idx, int val) { arr[idx] = val; }
@@ -158,7 +111,120 @@ DynArray arrFactory(int arrSize)
 	DynArray arr{ arrSize };
 	arr.randomize();
 	return arr;
-}
+}*/
+
+/*// заданные по умолчанию и удаленные методы
+class Point
+{
+	int x;
+	int y;
+	
+public:
+	Point() = default;
+	Point(int pX, int pY) : x{ pX }, y{ pY } {}
+	Point& setX(int pX) { x = pX; return *this; }
+	Point& setY(int pY) { y = pY; return *this; }
+	/*Point& setX(double pX) = delete;
+	Point& setY(double pY) = delete;
+	void showPoint() const
+	{
+		std::cout << '(' << x << ',' << y << ')';
+	}
+};
+
+int Max(int a, int b) { return a > b ? a : b; }
+template <typename T1, typename T2> int Max(T1 a, T2 b) = delete;*/
+
+class MedalRow
+{
+	char country[4];
+	int medals[3];
+public:
+	static const int GOLD{ 0 };
+	static const int SILVER{ 1 };
+	static const int BRONZE{ 2 };
+	MedalRow(const char* countryP, const int* medalsP)
+	{
+		strcpy_s(country, 4, countryP ? countryP : "NON");
+		for (int i = 0; i < 3; i++)
+		{
+			medals[i] = medalsP ? medalsP[i] : 0;
+		}
+	}
+	MedalRow() : MedalRow(nullptr, nullptr){}
+	MedalRow& setCountry(const char* countryP)
+	{
+		if (countryP)
+			strcpy_s(country, 4, countryP);
+		return *this;
+	}
+	const char* getCountry()const { return country; }
+	int& operator[](int idx)
+	{
+		assert((idx >= 0 and idx < 3) and "Index is out of range!");
+		return medals[idx];
+	}
+	int operator[](int idx) const
+	{
+		assert((idx >= 0 and idx < 3) and "Index is out of range!");
+		return medals[idx];
+	}
+	void Print() const
+	{
+		cout << '[' << country << "] - ( ";
+		for (int i = 0; i < 3; i++)
+		{
+			cout << medals[i];
+			if (i < 2) { cout << "\t"; }
+		}
+		cout << ")" << endl;
+	}
+};
+
+class MedalsTable
+{
+public:
+	static const int maxSize{ 10 };
+private:
+	MedalRow medalRows[MedalsTable::maxSize];
+	int size;
+	int findCountry(const char* country) const
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (strcmp(medalRows[i].getCountry(), country) == 0)
+				return i;
+		}
+		return -1;
+	}
+public:
+	MedalsTable() : size{ 0 } {}
+	MedalRow& operator[](const char* country)
+	{
+		int idx{ findCountry(country) };
+		if (idx == -1)
+		{
+			assert(size < MedalsTable::maxSize and "Table is Full!");
+			idx = size++;
+			medalRows[idx].setCountry(country);
+		}
+		return medalRows[idx];
+	}
+	const MedalRow& operator[](const char* country) const
+	{
+		int idx{ findCountry(country) };
+		assert(idx != -1 and "Country not found on const table");
+
+		return medalRows[idx];
+	}
+	void Print() const
+	{
+		for (int i = 0; i < size; i++)
+		{
+			medalRows[i].Print();
+		}
+	}
+};
 
 int main()
 {
@@ -167,13 +233,20 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 	
-	
-	DynArray ar1;
-	ar1 = arrFactory(10);
-	std::cout << "ar1 elements: ";
-	ar1.print();
-	
-	
+	MedalsTable mt;
+	cout << "Medal table #1\n";
+	mt["RUS"][MedalRow::GOLD] = 15;
+	mt["RUS"][MedalRow::SILVER] = 12;
+	mt["KAZ"][MedalRow::GOLD] = 7;
+	mt["KAZ"][MedalRow::BRONZE] = 15;
+	mt["BEL"][MedalRow::SILVER] = 15;
+	mt["BEL"][MedalRow::BRONZE] = 3;
+	mt["BEL"][MedalRow::GOLD] = 5;
+	mt.Print();
 
+	cout << "Medal table #2\n";
+	const MedalsTable mt1{ mt };
+	mt1.Print();
+	
 	return 0;
 }
