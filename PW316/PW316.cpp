ccd2dc633c311737e3a6290b885449757c8cbac8
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-05-16 20:54:30 +0300

    Virtual method
    Virtual method

--- PW316/PW316.cpp
+++ PW316/PW316.cpp
@@ -10,247 +10,75 @@
 #include <sys/locking.h>
 #include "BinaryTree.h"
 using namespace std;
-/*// Глобальный класс (А)
-class A
+
+/*class A
 {
 public:
-	int init(int);
-private:
-	class B
-	{
-	public:
-		//friend class A; //объявление друга
-		B(int val = 0); //Конструктор
-		void mf( A&);
-		int value;
-		static int static_mem;
-	};
-	//class Ref {
-	//	B* pli; //pli - имеет тип A::B*
-	//};
-	//class B {
-	//	Ref* pRef; // pRef - имеет тип A::Ref*
-	//};
-	//B num; 
-	// //Компилятор пока не видит определения 
-	// вложенного класса и не может создать объект
-	//поэтому можно создавать только ссылки и указатели
+	virtual void v_function();//описывает некое поведение класса А
+};
+class B : public A
+{
+	//Заменяющая функция описывает некое новое поведение класса B
+	virtual void v_function(void);
+};
+class C : public B
+{
+
 };
 
-void A::B::mf( A& i1)
+class Weapon
 {
-	int memb = i1.init(5);
-}
-A::B::B(int val = 0)
-{
-	//A::init() - ошибка нестатическое поле класса А должно использоваться через
-	//объект или указатель на тип А
-	value = val;
-}
-
-int A::B::static_mem = 1024;
-
-class B
-{
+	int TypeOfWeapon;
 public:
-	//Вложенный класс А, Он инкапсулирован внутри области видимости класса В
-	class A {};
-
-	A* obj; //Тут используется вложенный класс А
-};*/
-
-/*//класс "точка"
-class Point {
-	//кооординаты
-	int X;
-	int Y;
-public:
-	//конструктор
-	Point() {
-		X = Y = 0;
-	}
-	//установка координат
-	void SetPoint(int iX, int iY) {
-		X = iX;
-		Y = iY;
-	}
-	//демонстрация координат
-	void Show() {
-		cout << "----------------------------\n\n";
-		cout << X << "\t" << Y << "\n\n";
-		cout << "----------------------------\n\n";
-	}
+	virtual void Use1(void);//левая кнопка мыши
+	virtual void Use2(void);//правая кнопка мыши
 };
-//класс фигура
-class Figura {
-	//агрегация точки
-	//(координаты углов)
-	Point* obj;
-	//количество углов
-	int count;
-	//цвет фигуры
-	int color;
-public:
-	//конструктор
-	Figura() {
-		count = color = 0;
-		obj = NULL;
-	}
-	//создание фигуры
-	void CreateFigura(int cr, int ct) {
-		//если углов меньше трех — это не фигура
-		if (ct < 3) exit(0);
-		//инициализация цвета и количества углов
-		count = ct;
-		color = cr;
-		//выдделение памяти под массив точек
-		obj = new Point[count];
-		if (!obj) exit(0);
-		//установка координат точек
-		int tempX, tempY;
-		for (int i = 0; i < count; i++) {
-			cout << "Set X\n";
-			cin >> tempX;
-			cout << "Set Y\n";
-			cin >> tempY;
-			obj[i].SetPoint(tempX, tempY);
-		}
-	}
-	//показ фигуры
-	void ShowFigura() {
-		cout << "----------------------------\n\n";
-		cout << "Color" << color << "\n\nPoints — "<<count<<"\n\n";
-			for (int i = 0; i < count; i++) {
-				obj[i].Show();
-			}
-	}
-	//если фигура была очистить память
-	~Figura() {
-		if (obj != NULL) delete[]obj;
-	}
-};*/
+Weapon* Arms[10];//массив указателей на объекты Weapon*/
 
-/*
-* class имя_класса : спецификатор_наследования имя_базового_класса
-* { описание_класса }
-*/
-
-class Flat
+class Animal
 {
-	int numer_Flat; //номер квартиры 
-	int Human_live; // количесво жильцов
-	int num_rooms; //кол-во комнат
 public:
-	Flat()
+	char Title[30]; //Кличка животного
+	Animal(const char* t) { strcpy_s(Title, t); } //Конструктор
+	virtual void speak() = 0; //Чисто виртуальная функция
+};
+
+class Frog :public Animal
+{
+public:
+	Frog(const char* Title) : Animal(Title) {};
+	virtual void speak()
 	{
-		numer_Flat = 0;
-		Human_live = 0;
-		num_rooms = 0;
-	}
-	Flat(int Inumer_Flat, int IHuman_live, int Inum_rooms)
-	{
-		numer_Flat = Inumer_Flat;
-		Human_live = IHuman_live;
-		num_rooms = Inum_rooms;
-	}
-	void display()
-	{
-		cout << "Nuber Flatt: " << numer_Flat << endl;
-		cout << "Count human: " << Human_live << endl;
-		cout << "Count rooms: " << num_rooms << endl;
+		cout << Title << " Say " << "\'kwa-kwa\'" << endl;
 	}
 };
 
-class Home : public Flat
+class Dog :public Animal
 {
-	string name_street; // улица
 public:
-	Home(int Inumer_Flat, 
-		int IHuman_live, 
-		int Inum_rooms, 
-		string Iname_street) : Flat(Inumer_Flat, IHuman_live, Inum_rooms)
+	Dog(const char* Title) : Animal(Title) {};
+	virtual void speak()
 	{
-		name_street = Iname_street;
+		cout << Title << " Say " << "\'gav-gav\'" << endl;
 	}
-	void Print()
+};
+class Cat :public Animal
+{
+public:
+	Cat(const char* Title) : Animal(Title) {};
+	virtual void speak()
 	{
-		cout << "Street Home: " << name_street << endl;
-		display();
+		cout << Title << " Say " << "\'MMIIIAAAAAAAA-MMIIIAAAAAAAA\'" << endl;
 	}
 };
 
-class Roga
+class Lion :public Cat
 {
-protected:
-	char color[30];
-	int ves;
 public:
-	Roga()
+	Lion(const char* Title) : Cat(Title) {};
+	virtual void speak()
 	{
-		strcpy_s(color, "Brown");
-		ves = 10;
-	}
-	Roga(const char* c, int v)
-	{
-		strcpy_s(color, c);
-		ves = v;
-	}
-};
-
-class Kopyta
-{
-protected:
-	char forma[30];
-	int razmer;
-public:
-	Kopyta()
-	{
-		strcpy_s(forma, "Big");
-		razmer = 10;
-	}
-	Kopyta(const char* f, int r)
-	{
-		strcpy_s(forma, f);
-		razmer = r;
-	}
-};
-
-class Los : public Roga, public Kopyta
-{
-	char name[250];
-public:
-	Los(const char* n, const char* c, int v, const char* f, int r) :
-		Kopyta( f, r), Roga( c, v)
-	{
-		strcpy_s(name, n);
-	}
-	void Print()
-	{
-		cout << "Name: " << name << endl;
-		cout << "Forma: " << forma << endl;
-		cout << "Color: " << color << endl;
-		cout << "Ves Rogov: " << ves << endl;
-		cout << "Razmer kopyt: " << razmer << endl;
-		
-	}
-};
-
-class Buf : public Roga, public Kopyta
-{
-	char name[250];
-public:
-	Buf(const char* n, const char* c, int v, const char* f, int r) :
-		Kopyta(f, r), Roga(c, v)
-	{
-		strcpy_s(name, n);
-	}
-	void Print()
-	{
-		cout << "Name: " << name << endl;
-		cout << "Forma: " << forma << endl;
-		cout << "Color: " << color << endl;
-		cout << "Ves Rogov: " << ves << endl;
-		cout << "Razmer kopyt: " << razmer << endl;
+		cout << Title << " Say " << "\'RRRR-RRRRR\'" << endl;
 	}
 };
 
@@ -261,20 +89,26 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 
-	Los los("Osas", "Purple", 20, "Krug", 15);
-	los.Print();
-	cout << endl << "_____________________" << endl;
-	Buf buf("Pen", "Orange", 40, "Dual", 30);
-	buf.Print();
-	
+	Animal* animals[4] =
+	{
+		new Dog("Pich"),
+		new Cat("Gav"),
+		new Frog("Vasya"),
+		new Lion("Pushistick")
+	};
+	for (int i = 0; i < 4; i++)
+	{
+		animals[i]->speak();
+	}
 
-	/*Figura f;
-	f.CreateFigura(255, 3);
-	f.ShowFigura();*/
+	/*A object_A;
+	B object_B;
+	C object_C;
 
-	/*// Вложенный класс А не видим в данной области видимости
-	//Тут у нас используется глобальный класс А
-	A* obj2;*/
+	A* point_To_obj; //Указатель на базовый класс
+	point_To_obj = &object_C; //Присваиваем адрес класса потомка, потомка
+	point_To_obj = &object_B;//Присваиваем адрес класса потомка
+	point_To_obj->v_function();*/
 
 	return 0;
 }
