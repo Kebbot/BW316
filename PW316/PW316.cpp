6eabd2a08ef6da488811c7a5c5989a0f8a4ddbd7
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-05-18 21:10:55 +0300

    Virtual des
    Virtual des

--- PW316/PW316.cpp
+++ PW316/PW316.cpp
@@ -14,73 +14,62 @@ using namespace std;
 /*class A
 {
 public:
-	virtual void v_function();//описывает некое поведение класса А
+	int val;
 };
-class B : public A
-{
-	//Заменяющая функция описывает некое новое поведение класса B
-	virtual void v_function(void);
-};
-class C : public B
-{
-
-};
-
-class Weapon
-{
-	int TypeOfWeapon;
-public:
-	virtual void Use1(void);//левая кнопка мыши
-	virtual void Use2(void);//правая кнопка мыши
-};
-Weapon* Arms[10];//массив указателей на объекты Weapon*/
-
-class Animal
+class B : public virtual A {};
+class C : public virtual A {};
+class D : public B, public C 
 {
 public:
-	char Title[30]; //Кличка животного
-	Animal(const char* t) { strcpy_s(Title, t); } //Конструктор
-	virtual void speak() = 0; //Чисто виртуальная функция
-};
-
-class Frog :public Animal
-{
-public:
-	Frog(const char* Title) : Animal(Title) {};
-	virtual void speak()
+	int Get_val()
 	{
-		cout << Title << " Say " << "\'kwa-kwa\'" << endl;
+		return val;
+	}
+};*/
+
+class base
+{
+	char* sp1;
+	int size;
+public:
+	base(const char* S, int s)
+	{
+		size = s;
+		sp1 = new char[size];
+		strcpy_s(sp1,size, S);
+	}
+	virtual ~base()
+	{
+		cout << "Base" << endl;
+		delete[] sp1;
 	}
 };
 
-class Dog :public Animal
+class Derived : public base
 {
+	char* sp2;
+	int size2;
 public:
-	Dog(const char* Title) : Animal(Title) {};
-	virtual void speak()
+	Derived(const char* S1, int s1,
+		const char* S2, int s2) : base(S1, s1)
 	{
-		cout << Title << " Say " << "\'gav-gav\'" << endl;
+		size2 = s2;
+		sp2 = new char[size2];
+		strcpy_s(sp2, size2, S2);
 	}
-};
-class Cat :public Animal
-{
-public:
-	Cat(const char* Title) : Animal(Title) {};
-	virtual void speak()
+	virtual ~Derived()
 	{
-		cout << Title << " Say " << "\'MMIIIAAAAAAAA-MMIIIAAAAAAAA\'" << endl;
+		cout << "Derived" << endl;
+		delete[] sp2;
 	}
 };
 
-class Lion :public Cat
+class Something
 {
 public:
-	Lion(const char* Title) : Cat(Title) {};
-	virtual void speak()
-	{
-		cout << Title << " Say " << "\'RRRR-RRRRR\'" << endl;
-	}
+	virtual ~Something() = 0;
 };
+Something::~Something() {};
 
 int main()
 {
@@ -89,26 +78,24 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 
-	Animal* animals[4] =
+	
+	//Derived myStr("string 1", 9, "string 2", 9);
+	/*base* pBase;
+	pBase = new Derived("string 1", 9, "string 2", 9);
+	delete pBase;*/
+
+	// try - определяет блок кода где может возникнуть ошибка
+	//throw - выводит программу из "исключительной" ситуации
+	// catch - ловит, анализирует и выдет нужную реакцию
+	try
 	{
-		new Dog("Pich"),
-		new Cat("Gav"),
-		new Frog("Vasya"),
-		new Lion("Pushistick")
-	};
-	for (int i = 0; i < 4; i++)
-	{
-		animals[i]->speak();
+		//Блок кода 
+		throw //выражение_определенного_типа
 	}
-
-	/*A object_A;
-	B object_B;
-	C object_C;
-
-	A* point_To_obj; //Указатель на базовый класс
-	point_To_obj = &object_C; //Присваиваем адрес класса потомка, потомка
-	point_To_obj = &object_B;//Присваиваем адрес класса потомка
-	point_To_obj->v_function();*/
+	catch(//тип_исключения имя)
+	{
+		//блок анализа
+	}
 
 	return 0;
 }
