ac81741fd5163ee591d9ab22f0b3c2710cb7d324
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-05-23 19:09:08 +0300

    Stream
    Stream

--- PW316/PW316.cpp
+++ PW316/PW316.cpp
@@ -14,14 +14,162 @@ using namespace std;
 #define NUM_COLS 18
 //Кол-во строк на экране
 #define NUM_ROWS 24
-int main()
+void main()
 {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 	
-	char path[MAX_PATH];
+	//ifstream - для чтения с файла
+	//ofstream - для записи в файл
+	//fstream - совмещает запись и чтение
+
+	//open(путь_к_файлу);
+	//open(путь_к_файлу, режим_открытия);
+
+	//ios::in - открытие файла для ввода (чтения), 
+	// можно поставить только для ifstream и fstream
+	ifstream in; //поток для чтения
+	in.open("Hello.txt"); // открытие файла для чтения
+
+	//ios::out - открытие файла для вывода (Записи), старые данные удаляются, 
+	// можно поставить только для ofstream и fstream
+	ofstream out; //поток для записи
+	out.open("Hello1.txt"); // открытие файла для записи
+
+	//ios::app - открытие для дозаписи в файл, старые данные не удаляются
+	ofstream out2; //поток для записи
+	out2.open("Hello2.txt", ios::app); // открытие файла для дозаписи
+
+	//ios::ate - после открытия файла  перемещает указатель в конец файла
+
+	//ios::trunc - файл усекается при открытии, (обнуление файла)
+	ofstream out3; //поток для записи
+	out2.open("Hello2.txt", ios::out | ios::trunc); //установка нескольких режимов
+
+	//ios::binary - открывает файл в бинарном режиме
+
+
+	fstream fs;
+	fs.open("Hello3.txt");
+
+	ofstream out4("Hello4.txt");
+	ifstream in2("Hello5.txt");
+	fstream fs2("Hello6.txt");
+	if (fs2.is_open()) // если файл открыт
+	{
+		fs2.close();
+	}
+
+
+
+	/*char Answer;
+	const int MessageCount = 8;
+	int i, j;
+	//Подсказки
+	enum {
+		CHOICE = 3, INPUT_FILENAME,
+		INPUT_DIMENSIONS, INPUT_ELEMENTS,
+		FILE_ERROR
+	};
+	//Сообщения
+	char Msg[MessageCount][100]
+	{
+		"1. Вывести данные из текстового файла\n",
+		"2. Записать данные в текстовый файл\n",
+		"3. Выход из программы\n",
+		"\nВаш выбор: ",
+		"Введите имя обрабатываемого файла: ",
+		"Введите размерности матрицы:\n",
+		"Введите элементы матрицы:\n",
+		"Невозможно открыть файл\n"
+	};
+	////Русификация сообщений и вывод меню на экран
+	//for (i = 0; i < MessageCount; i++)
+	//	CharToOem(Msg[i], Msg[i]);
+	do
+	{
+		for (int i = 0; i < 4; i++)
+			cout << Msg[i];
+		cin >> Answer;
+	} while (Answer < '1' || Answer > '3');
+	if (Answer == '3')
+		return;
+	//Переменная для имени файла
+	char FileName[80];
+	//Размерности матрицы
+	int M, N;
+	int num;
+	cout << "\n" << Msg[INPUT_FILENAME];
+	cin >> FileName;
+	//Если выбран первый пункт меню,
+	//то выводим данные из текстового файла на экран
+	if (Answer == '1')
+	{
+		//Если файл с указанным именем не существует,
+		//выводим сообщение об ошибке
+		ifstream inF(FileName, ios::in | ios::_Nocreate);
+		if (!inF)
+		{
+			cout << endl << Msg[FILE_ERROR];
+			return;
+		}
+		//Считываем размерность массива
+		inF >> M;
+		inF >> N;
+		//Считываем элементы массива из файла и
+		//выводим их сразу на экран
+		for (i = 0; i < M; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+				inF >> num;
+				cout << setw(6) << num;
+			}
+			cout << endl;
+		}
+		inF.close();
+	}
+	//Если выбран второй пункт меню, то запрашиваем
+	//у пользователя данные и выводим их в текстовый
+	//файл
+	else
+	{
+		//Открываем файл для записи.
+		//Если файл с указанным именем не существует,
+		//то программа создает его
+		ofstream outF(FileName, ios::out);
+		if (!outF)
+		{
+			cout << "\n" << Msg[FILE_ERROR];
+			return;
+		}
+		//Запрашиваем размерность матрицы
+		//и записываем данные в файл
+		cout << Msg[INPUT_DIMENSIONS];
+		cout << "M: ";
+		cin >> M;
+		cout << "N: ";
+		cin >> N;
+		outF << M << ' ' << N << "\n";
+		cout << Msg[INPUT_ELEMENTS];
+		//Запрашиваем элементы массива и записываем
+		//их в файл
+		for (i = 0; i < M; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+				cout << "A[" << i << "][" << j << "] = ";
+				cin >> num;
+				outF << num << " ";
+			}
+			outF << "\n";
+		}
+		outF.close();
+	}*/
+
+	/*char path[MAX_PATH];
 	//запрашиваем путь к файлу
 	cout << "Input path to file: ";
 	cin.getline(path, MAX_PATH);
@@ -70,20 +218,19 @@ int main()
 				cout << ".";
 		cout << "\n";
 		//Если экран уже заполнен,
-//производим остановку
+		//производим остановку
 		if (++counter == NUM_ROWS)
 		{
 			counter = 0;
 			cout << "Press any key to continue..." <<
 				flush;
 			//Ожидаем нажатия любой клавиши
-			getch();
+			_getch();
 			cout << "\n";
 		}
 	}
 	//Закрываем файл
-	input.close();
+	input.close();*/
 	
-
-	return 0;
+	
 }
